af03680c4d364b45e98e42f8038ba79fea2844a9
Ezjq <kokieunz@gmail.com>
2025-05-02 06:55:40 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ import json
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "0x4AAAAAABWfDQXfye-8ewXoXpq-SQj5iF0")  # Add your own secure key here
 
-SCRIPTS_DIR = "scripts"
+SCRIPTS_DIR = "/tmp/scripts"
 
 # Create scripts folder if it doesn't exist
 os.makedirs(SCRIPTS_DIR, exist_ok=True)
