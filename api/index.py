258bdf7a817cdca428961106c1c1605365906187
Ezjq <kokieunz@gmail.com>
2025-05-02 07:01:44 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ import uuid
 import json
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("FLASK_SECRET_KEY", "0x4AAAAAABWfDQXfye-8ewXoXpq-SQj5iF0")  # Add your own secure key here
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "0x4AAAAAABYpbj6-6zZXa_K-qclTpaXWyyM")  # Add your own secure key here
 
 SCRIPTS_DIR = "/tmp/scripts"
 
